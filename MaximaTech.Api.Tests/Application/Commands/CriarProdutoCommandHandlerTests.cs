256d1962989df9796112767c7a6d9b8967ba45a9
Rubens Facco <rubensfacco94@gmail.com>
2024-03-25 09:27:35 -0300

    Feat: Adição de projeto de testes, com dois testes. Ambos passando.

---
+++ MaximaTech.Api.Tests/Application/Commands/CriarProdutoCommandHandlerTests.cs
@@ -0,0 +1,43 @@
+﻿using MaximaTech.api.Models;
+using MaximaTech.Application.Command;
+using MaximaTech.core.Interfaces;
+
+using Moq;
+
+using Xunit;
+
+namespace MaximaTech.Api.Tests.Application.Commands
+{
+    public class CriarProdutoCommandHandlerTests
+    {
+
+        [Test]
+        public async Task InputDataIsOk_Executed_ReturnProductId()
+        {
+            var uow = new Mock<IUnitOfWork>();
+            var produtoRepository = new Mock<IProdutoRepository>();
+
+            uow.SetupGet(uow => uow.ProdutoRepository).Returns(produtoRepository.Object);
+
+            var criarProdutoCommand = new CriarProdutoCommand(
+                new api.Models.ProdutoViewModel()
+                {
+                    Codigo = "0010",
+                    DepartamentoId = new Guid("a9efa82f-e596-41e0-a77e-168eac7b2d17"),
+                    Descricao = "Meu Teste",
+                    Status = true,
+                    Preco = 8
+                }
+                );
+
+            var criarProdutosCommandHandler = new CriarProdutoCommandHandler(uow.Object);
+
+            var produtoCriado = await criarProdutosCommandHandler.Handle(criarProdutoCommand, new CancellationToken());
+
+            Assert.IsNotNull(produtoCriado);
+
+            produtoRepository.Verify(pr => pr.Add(It.IsAny<Produtos>()), Times.Once);
+
+        }
+    }
+}
---
+++ MaximaTech.Api.Tests/Application/Queries/GetAllDepartamentosQueryHandlerTests.cs
@@ -0,0 +1,45 @@
+﻿using MaximaTech.api.Models;
+using MaximaTech.Application.Queries;
+using MaximaTech.core.Interfaces;
+
+using Moq;
+
+namespace MaximaTech.Api.Tests.Application.Queries
+{
+    public class GetAllDepartamentosQueryHandlerTests
+    {
+        [Test]
+        public async Task DadoQuatroDepartamentosExistestes_Executa_RetornaQuatroDepartamentos()
+        {
+            //arrange
+            var deptos = new List<Departamentos>()
+            {
+                new Departamentos(){Codigo = "010", Descricao = "BEBIDAS"},
+                new Departamentos(){Codigo = "020", Descricao = "CONGELADOS"},
+                new Departamentos(){Codigo = "030", Descricao = "LATICINIOS"},
+                new Departamentos(){Codigo = "040", Descricao = "VEGETAIS"}
+            };
+
+            var uowMock = new Mock<IUnitOfWork>();
+            var departamentosRepositoryMock = new Mock<IDepartamentoRepository>();
+            departamentosRepositoryMock.Setup(pr => pr.GetAll().Result).Returns(deptos);
+
+            uowMock.SetupGet(uow => uow.DepartamentoRepository).Returns(departamentosRepositoryMock.Object);
+
+            var getAllDepartamentosQuery = new GetAllDepartamentosQuery();
+            var getAllDepartamentosQueryHandler = new GetAllDepartamentosQueryHandler(uowMock.Object);
+
+            //act
+            var departamentos = await getAllDepartamentosQueryHandler.Handle(getAllDepartamentosQuery, new CancellationToken());
+
+            // Assert
+            Assert.NotNull(departamentos);
+            Assert.IsNotEmpty(departamentos);
+            Assert.That(departamentos.Count, Is.EqualTo(deptos.Count));
+
+            departamentosRepositoryMock.Verify(pr => pr.GetAll().Result, Times.Once);
+
+
+        }
+    }
+}
